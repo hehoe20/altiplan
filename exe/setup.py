fb90e95238bd9ecbc9f2bb60869ce66edb29caf9
Henrik Højgaard <73895114+hehoe20@users.noreply.github.com>
2026-02-18 18:38:31 +0100

    Add files via upload

---
+++ exe/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import py2exe
+
+options = {
+    "py2exe": {
+        "packages": [
+            "lxml",
+            "requests",
+            "bs4",
+        ],
+        "includes": [
+            "lxml._elementpath",
+            # requests kan trække ekstra ind; typisk fanger packages det,
+            # men denne hjælper nogle gange:
+            "requests",
+            "bs4",
+        ],
+        # Hvis du vil undgå SSL/cert issues, kan certifi være relevant:
+        # "packages": [..., "certifi"],
+    }
+}
+
+setup(
+    console=["altiplan.py"],   # tilpas til dit entry script
+    options=options,
+)
